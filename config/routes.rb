eb508286ac987a69d9c10cc97a91494983ff4567
EC2 Default User <specialized.19991129@gmail.com>
2022-10-22 10:14:36 +0900

    [admin]商品一覧ページ作成中

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,18 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+
+  namespace :admin do
+   root :to => 'top#top'
+   resources :items, only: [:show, :index, :new, :create, :edit, :update]
+  end
+
 end
+
