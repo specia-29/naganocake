3e09308287199d3aac7a06c48293893d24be2575
EC2 Default User <specialized.19991129@gmail.com>
2022-11-02 15:31:55 +0900

    [admin]admin/items/new 作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,6 +10,27 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path(@item)
+    else
+      redirect_to new_admin_item_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path(@item)
+      flash[:notice_update] = "ジャンル情報を更新しました！"
+    else
+      redirect_to edit_admin_item_path(@item)
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :description, :price_without_tax, :image, :is_sales_status)
   end
 
 end
