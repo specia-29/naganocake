283b12ced91fa84cf6dd16269468289cf9d64be1
EC2 Default User <specialized.19991129@gmail.com>
2022-11-09 16:29:08 +0900

    [Add]public/customers show,edit 退会機能未着手

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,29 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_mypage_path
+      flash[:notice_update] = "ジャンル情報を更新しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def withdrawal
+  end
+
+  def stat_update
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
-
   }
 
   # 管理者用
@@ -19,6 +18,9 @@ Rails.application.routes.draw do
     root :to => 'homes#top'
     get '/about', to: 'homes#about'
     get '/items', to: 'items#index'
+    get '/customers/mypage', to: 'customers#show'
+    patch '/customers/mypage', to: 'customers#update'
+    get '/public/customers/edit', to: 'customers#edit'
   end
 
   namespace :admin do
