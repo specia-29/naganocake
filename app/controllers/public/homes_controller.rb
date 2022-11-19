881e85f33e6b366aa90e35efd151691239d415c9
EC2 Default User <specialized.19991129@gmail.com>
2022-11-19 12:07:37 +0900

    [Update]public新規登録・ログインページレイアウト調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
 
   def about
