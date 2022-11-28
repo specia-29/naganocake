45930fd8790793290c308732c08464d14d968ae4
EC2 Default User <specialized.19991129@gmail.com>
2022-11-28 18:34:02 +0900

    [Update]カート内削除機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -13,7 +13,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
-    @cart_item = current_customer.cart_items
+    @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_item_path
   end
