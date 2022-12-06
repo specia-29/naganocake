cb1fc71a3ac120ca3b138da07fccafe8ccc3ebd3
EC2 Default User <specialized.19991129@gmail.com>
2022-12-06 21:05:34 +0900

    [Finish]制作ステータスと注文ステータスの連携

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,18 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details
+
     @order_detail.update(order_detail_params)
+    if @order_details.where(making_status: 2).count >= 1
+      @order.status = 2
+      @order.save
+    end
+    if @order_details.count == @order_details.where(making_status: 3).count
+      @order.status = 3
+      @order.save
+    end
       redirect_to admin_order_path(@order_detail.order.id)
       flash[:notice_update] = "ステータス情報を更新しました！"
   end
